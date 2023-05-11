c8f46e38360d8f73f9220960200c42228443ca41
saba-bar95 <s.barbakadze95@gmail.com>
2023-05-11 14:42:28 +0400

    Add more UI & functionalities

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-const container = document.querySelector(".container");
-const divWidth = 320 / 16;
-const divHeight = 320 / 16;
+const input = document.querySelector("input");
+const gridContainter = document.querySelector(".grid-container");
+const colorBtns = document.querySelector(".color-buttons");
+const random = document.querySelector(".random");
+let divs;
 
-function createHover(div) {
-  div.forEach((div) =>
+const randomRgbColor = function () {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
+};
+
+function createHover(divs, e) {
+  let targetValue;
+  if (e === undefined) targetValue = true;
+  else targetValue = e.target.classList.contains("black");
+  divs.forEach((div) =>
     div.addEventListener("mouseover", function (e) {
-      e.target.style.backgroundColor = "black";
+      if (targetValue) {
+        e.target.style.backgroundColor = "black";
+      } else {
+        e.target.style.backgroundColor = randomRgbColor();
+      }
     })
   );
 }
 
-function createSquares() {
-  for (let i = 1; i <= 256; i++) {
+function createGrid(val = 16) {
+  gridContainter.textContent = "";
+  const divWidth = 500 / val;
+  const divHeight = 500 / val;
+  for (let i = 1; i <= val * val; i++) {
     const div = document.createElement("div");
     div.classList.add("grid-square");
     div.style.width = divWidth + "px";
     div.style.height = divHeight + "px";
-    container.appendChild(div);
+    gridContainter.appendChild(div);
   }
-
-  const divs = document.querySelectorAll(".grid-square");
+  divs = document.querySelectorAll(".grid-square");
   createHover(divs);
 }
 
-createSquares();
+input.addEventListener("click", function (e) {
+  const value = input.value;
+  createGrid(value);
+});
+
+colorBtns.addEventListener("click", function (e) {
+  if (e.target.classList.contains("reset")) {
+    divs.forEach((div) => (div.style.backgroundColor = "white"));
+    return;
+  }
+  createHover(divs, e);
+});
+
+createGrid();
